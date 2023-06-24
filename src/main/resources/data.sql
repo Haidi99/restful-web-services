e9bf413542fc60c076c4d434b31673cc940eba57
Haidi99 <haidi.hany48@gmail.com>
2023-06-24 21:09:42 +0300

    connect with H2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user_details(birth_date,id,name) values (current_date(),10001,'Haidi');
+insert into user_details(birth_date,id,name) values (current_date(),10002,'Toka');
+insert into user_details(birth_date,id,name) values (current_date(),10003,'Mona');
+insert into user_details(birth_date,id,name) values (current_date(),10004,'Mayar');
