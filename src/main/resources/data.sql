71edc731239702b5df9d6b4af03f5d843a30a326
Haidi99 <haidi.hany48@gmail.com>
2023-06-24 22:23:35 +0300

    create the post entity and insert data into it

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@ insert into user_details(birth_date,id,name) values (current_date(),10001,'Haidi
 insert into user_details(birth_date,id,name) values (current_date(),10002,'Toka');
 insert into user_details(birth_date,id,name) values (current_date(),10003,'Mona');
 insert into user_details(birth_date,id,name) values (current_date(),10004,'Mayar');
+insert into post(id,user_id,description) values(20001,10001,'I want to leave my house');
+insert into post(id,user_id,description) values(20002,10001,'I want to graduate');
+insert into post(id,user_id,description) values(20003,10002,'I want to make an order');
+insert into post(id,user_id,description) values(20004,10002,'I want to go to Jannah <3');
